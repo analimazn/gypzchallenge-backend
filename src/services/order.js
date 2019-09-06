54eb6a7c184e2ece2562c8db501d387302710414
analimazn <analimazn@gmail.com>
2019-09-06 19:28:04 -0300

    Separated object receive from db to format JSON into formatJson

--- src/services/order.js
+++ src/services/order.js
@@ -3,11 +3,19 @@ const moment = require('moment')
 const config = require('../config/order')
 
 module.exports = {
+  async index() {
+    try {
+      const result = ''
+      return result
+    } catch (err) {
+      throw err
+    }
+  },
   async find() {
     try {
       const result = await Order
         .find({})
-        .sort({ createdAt: 'asc', firstName: 'asc' })
+        .sort({ createdAt: 'desc', firstName: 'asc' })
       return result
     } catch (err) {
       throw err
@@ -15,33 +23,8 @@ module.exports = {
   },
   async create(req) {
     try {
-      const date = moment.utc(`${req.body.bornDate}`, "DD/MM/YYYY")
-      const score = await config.generateScore()
-      const creditCard = await config.approveCreditCard(score, req.body.amount)
-      
-      const order = await new Order({
-        user: {
-          firstName: req.body.firstName,
-          lastName: req.body.lastName,
-          documentNumber: req.body.documentNumber,
-          bornDate: date,
-        },
-        contact: {
-          cellphoneNumber: req.body.cellphoneNumber,
-          phoneNumber: req.body.phoneNumber,
-        },
-        address: {
-          postCode: req.body.postCode,
-          houseNumber: req.body.houseNumber,
-          moreInfo: req.body.moreInfo,
-        },
-        creditCardInfo: {
-          amount: req.body.amount,
-          approved: creditCard.approved,
-          creditLimit: creditCard.creditLimit
-        },
-        createdAt: new Date()
-      })
+      const json = await config.formatJson(req)
+      const order = new Order(json)
       const result = await order.save()
       return result
     } catch (err) {
@@ -51,7 +34,7 @@ module.exports = {
   async findOne(req) {
     try {
       const result = await Order.findOne({
-        _id: req.body._id
+        _id: req.body.data._id
       })
       return result
     } catch (err) {
@@ -61,7 +44,7 @@ module.exports = {
   async remove(req) {
     try {
       const result = await Order
-        .findById(req.body._id)
+        .findById(req.query._id)
         .deleteOne()
       return result
     } catch (err) {
